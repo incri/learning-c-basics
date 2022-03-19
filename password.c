e2e0e224b5e11281b493d7824b3f277c1dbaf862
Baibhav <paudelbaibhav0@gmail.com>
2022-03-20 00:00:11 +0545

    added limit func

--- password.c
+++ password.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main ()
-{
+int main (){
+
 	int password = 54546;
 	int guess;
-
-	while (guess != password ){
-		printf("enter a number : ");
-		scanf("%d",&guess);
-	}
-	printf("you got it boy!");
-	return 0;
-	}
+	int guesscount = 0;
+	int guessLimit = 3;
+	int outofguess = 0;
+	
+while (guess != password && outofguess == 0 ){
+if (guesscount < guessLimit){
+printf("enter a number : ");
+scanf("%d",&guess);
+guesscount++;
+}
+else { outofguess = 1;
+}
+}
+if (outofguess ==1){
+printf("wrong password" );
+}else {
+printf("you got it boy!");
+}
+return 0;
+}
