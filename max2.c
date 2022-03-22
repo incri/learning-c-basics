f24b17642bdc9865e67af0d9118b9f4f509c367a
Baibhav <paudelbaibhav0@gmail.com>
2022-03-22 14:21:48 +0545

    arry count

--- max2.c
+++ max2.c
@@ -3,11 +3,12 @@ int main()
 {
 
 int n[5]={1, 7, 3, 9, 6};
+int l= sizeof n/ sizeof(n[0]);
 int i;
 int greatest;
 
 greatest = n[0];
-for (i=0; i<5; i++){
+for (i=0; i<l; i++){
 
 	if (n[i]> greatest){
 
